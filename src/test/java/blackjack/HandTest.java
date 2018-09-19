4f3cc3efbf18b10b5d8ebede25075d86b87889b0
Altin R <altin.rex@gmail.com>
2018-09-19 15:34:18 -0400

    TEST: Added drawing card to hand test case

--- src/test/java/blackjack/HandTest.java
+++ src/test/java/blackjack/HandTest.java
@@ -7,4 +7,14 @@ public class HandTest extends TestCase {
 		Hand h1 = new Hand();
 		assertNotNull(h1);
 	}
+
+	public void testDrawCard() {
+		Deck d1 = new Deck();
+		d1.createDeck();
+
+		Hand h1 = new Hand();
+		h1.drawCard();
+
+		assertEquals("SK", h1.toString());
+	}
 }
