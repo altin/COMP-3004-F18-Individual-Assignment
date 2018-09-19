d22f8f96948243cd68e832720b2905f9429b4f30
Altin R <altin.rex@gmail.com>
2018-09-19 13:26:35 -0400

    CODE: Implemented getRank and getSuit in Card

--- src/main/java/blackjack/Card.java
+++ src/main/java/blackjack/Card.java
@@ -4,10 +4,30 @@ public class Card {
 	public enum Rank {
 		ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN,
 		KING;
+		
+		public int getValue() {
+			// Face Cards
+			if (this.ordinal() + 1 > 10) {
+				return 10;
+			// All other cards
+			} else {
+				return this.ordinal() + 1;
+			}
+		}
 	}
 	
 	public enum Suit {
-		CLUBS, DIAMONDS, HEARTS, SPADES;
+		CLUBS("C"), DIAMONDS("D"), HEARTS("H"), SPADES("S");
+
+		private final String symbol;
+
+		private Suit(String symbol) {
+			this.symbol = symbol;
+		}
+
+		public String getSymbol() {
+			return this.symbol;
+		}
 	}
 	
 	private Rank rank;
@@ -16,5 +36,13 @@ public class Card {
 	public Card(Rank rank, Suit suit) {
 		this.rank = rank;
 		this.suit = suit;
+	}
+	
+	public int getRank() {
+		return this.rank.getValue();
+	}
+	
+	public String getSuit() {
+		return this.suit.getSymbol();
 	}
 }
