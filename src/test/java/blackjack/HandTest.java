3db199e81ad568b0d3b117e1cdc9157448e7f18a
Altin R <altin.rex@gmail.com>
2018-09-20 04:35:07 -0400

    TEST: Add more test cases in HandTest

--- src/test/java/blackjack/HandTest.java
+++ src/test/java/blackjack/HandTest.java
@@ -33,4 +33,72 @@ public class HandTest extends TestCase {
 		assertEquals(12, h1.getScore());
 		
 	}
+	
+	public void testAceWorthOne() {
+		Deck d1 = new Deck();
+		
+		d1.addCard(new Card(Rank.ACE, Suit.CLUBS));
+		d1.addCard(new Card(Rank.KING, Suit.DIAMONDS));
+		d1.addCard(new Card(Rank.KING, Suit.HEARTS));
+		
+		Hand h1 = new Hand();
+		h1.drawCard(d1);
+		h1.drawCard(d1);
+		h1.drawCard(d1);
+		
+		// Two kings = 20, 1 ace = 1, therefore 21
+		assertEquals(21, h1.getScore());
+		
+	}
+	
+	public void testAceWorthEleven() {
+		Deck d1 = new Deck();
+		
+		d1.addCard(new Card(Rank.ACE, Suit.CLUBS));
+		d1.addCard(new Card(Rank.NINE, Suit.DIAMONDS));
+		
+		Hand h1 = new Hand();
+		h1.drawCard(d1);
+		h1.drawCard(d1);
+		
+		// Ace = 11, Nine = 9, total = 20
+		assertEquals(20, h1.getScore());
+		
+	}
+	
+	public void testTwoAcesWorthOneEach() {
+		Deck d1 = new Deck();
+		
+		d1.addCard(new Card(Rank.ACE, Suit.CLUBS));
+		d1.addCard(new Card(Rank.ACE, Suit.DIAMONDS));
+		d1.addCard(new Card(Rank.KING, Suit.HEARTS));
+		
+		Hand h1 = new Hand();
+		h1.drawCard(d1);
+		h1.drawCard(d1);
+		h1.drawCard(d1);
+		
+		// King = 10, Ace = 1, Ace = 1
+		assertEquals(12, h1.getScore());
+		
+	}
+	
+	public void testAceWorthElevenThenOne() {
+		Deck d1 = new Deck();
+		
+		d1.addCard(new Card(Rank.ACE, Suit.CLUBS));
+		d1.addCard(new Card(Rank.ACE, Suit.DIAMONDS));
+		d1.addCard(new Card(Rank.KING, Suit.HEARTS));
+		
+		Hand h1 = new Hand();
+		h1.drawCard(d1);
+		h1.drawCard(d1);
+		// King = 10, Ace = 11, therefore 21
+		assertEquals(21, h1.getScore());
+		
+		// King = 10, Ace = 1, Ace = 1
+		h1.drawCard(d1);
+		assertEquals(12, h1.getScore());
+		
+	}
 }
