81a59021c175ea77b8f8d071dbb6e18461b96fb9
Altin R <altin.rex@gmail.com>
2018-09-20 05:07:43 -0400

    REFACTOR: Added State enum, imported ArrayList

--- src/main/java/blackjack/Player.java
+++ src/main/java/blackjack/Player.java
@@ -1,6 +1,12 @@
 package blackjack;
 
+import java.util.ArrayList;
+
 public class Player {
+	public enum State {
+		BLACKJACK, BUST, STAND, NONE;
+	}
+	
 	protected String name = "Player";
 	protected ArrayList<Hand> hands;
 	protected ArrayList<Hand> standingHands;
