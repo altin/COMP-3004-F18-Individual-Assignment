8bd60ca3c18adc36767d9e82636485d988d7b0a8
Altin R <altin.rex@gmail.com>
2018-09-20 03:50:51 -0400

    TEST: Added test case to check value of a hand:

--- src/test/java/blackjack/HandTest.java
+++ src/test/java/blackjack/HandTest.java
@@ -1,5 +1,7 @@
 package blackjack;
 
+import blackjack.Card.Rank;
+import blackjack.Card.Suit;
 import junit.framework.TestCase;
 
 public class HandTest extends TestCase {
@@ -16,5 +18,19 @@ public class HandTest extends TestCase {
 		h1.drawCard(d1);
 
 		assertEquals("SK", h1.toString());
+	}
+	
+	public void testAceWorthOneOrEleven {
+		Deck d1 = new Deck();
+		
+		d1.addCard(new Card(Rank.ACE, Suit.CLUBS));
+		d1.addCard(new Card(Rank.ACE, Suit.DIAMONDS));
+		
+		Hand h1 = new Hand();
+		h1.drawHand(deck);
+		
+		// Two aces = value of 12 (1 ace is 11, the other is 1)
+		assertEquals(12, h1.getScore());
+		
 	}
 }
---
+++ src/test/java/blackjack/PlayerTest.java
@@ -0,0 +1,9 @@
+package blackjack;
+
+import junit.framework.TestCase;
+
+public class PlayerTest extends TestCase {
+	public void testConstructor() {
+		
+	}
+}
